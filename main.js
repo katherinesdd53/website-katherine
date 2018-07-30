0f25337c1b462167a784d31d9209fca50c39076d
katherine <katherinesdd53@gmail.com>
2018-07-30 15:39:43 -0400

    PHP inclusion

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-// $( document ).ready(function() {
-// });
-
-var express = require("express");
-var app     = express();
-var path    = require("path");
-
-app.use(express.static(__dirname + 'main.css')); //maybe add css/main.css
-
-app.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/index.html'));
+$( document ).ready(function() {
 });
 
-app.listen(process.env.PORT || 3000, function (){
-	console.log('Example app listening on port 3000!')
-})
+// var express = require("express");
+// var app     = express();
+// var path    = require("path");
+
+// app.use(express.static(__dirname + 'main.css')); //maybe add css/main.css
+
+// app.get('/',function(req,res){
+//   res.sendFile(path.join(__dirname+'/index.html'));
+// });
+
+// app.listen(process.env.PORT || 3000, function (){
+// 	console.log('Example app listening on port 3000!')
+// })
