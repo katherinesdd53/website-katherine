263a6d1d780d92e2515c11258b5f112503153638
katherine <katherinesdd53@gmail.com>
2018-07-30 01:30:12 -0400

    new folder

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ var express = require("express");
 var app     = express();
 var path    = require("path");
 
+app.use(express.static(__dirname + '/css'));
 
 app.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/index.html'));
