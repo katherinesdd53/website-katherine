be04dfb2db76bb20c206956473eab6d93b753bc5
katherine <katherinesdd53@gmail.com>
2018-07-30 00:44:07 -0400

    node start

--- main.js
+++ main.js
@@ -1,9 +1,2 @@
-var sequelize;
- if (process.env.DATABASE_URL) {
- sequelize = new Sequelize(process.env.DATABASE_URL);
- } else {
- sequelize = new Sequelize(config.database, config.username, config.password, config);
- }
-
-$( document ).ready(function() {
-});
+// $( document ).ready(function() {
+// });
